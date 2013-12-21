36e27c34fd2238c6ba8b3ca43295a35ec03e9fa6
jay16 <jay_li@intfocus.com>
2013-12-21 22:15:02 +0800

    auto commit - 2013/12/21-22:15:01

--- app/helpers/traffic_helper.rb
+++ app/helpers/traffic_helper.rb
@@ -136,7 +136,7 @@ module TrafficHelper
         "refer_to" => tmp_p,
 	"fill" => "red",
 	"title" => tmp_p,
-	"body" => "访客次数:#{info.ip_num.to_i}次<br>访客人次:#{info.ip_peo}人"
+	"body" => "访客次数:#{info.ip_num.to_i}次</br>访客人次:#{info.ip_peo}人"
       })
     end
     return chart_options
