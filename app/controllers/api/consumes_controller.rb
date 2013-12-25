af2499d60e7b7f364b0802a708559895a943c668
jay16 <jay_li@intfocus.com>
2013-12-25 14:15:01 +0800

    auto commit - 2013/12/25-14:15:01

--- app/controllers/api/consumes_controller.rb
+++ app/controllers/api/consumes_controller.rb
@@ -2,11 +2,28 @@
 class Api::ConsumesController < ApplicationController
   
   def create
-    render :json => params.to_json
+    ret, ret_info, consume = 0, "", {}
+    params.delete_if { |p| %w(action controller).include?(p) }
+    
+    if params.empty?
+      ret_info = "params is empty"
+    else
+      consume = User.find_by_email("android_app@solife.us")
+        .consumes.create(params[:consume]) 
+
+      if consume.save
+        ret, ret_info, consume = 1, "OK", consume.to_json
+      else
+        ret, ret_info = 0, consume.errors.full_messages
+      end
+    end
+
+    render :json => { :ret => ret, :ret_info => ret_info, :consume => consume}
   end
 
   def list
-    render :json => params.to_json
+    consumes = User.find_by_email("jay_li@xsolife.com").consumes
+    render :json => consumes.to_json
   end
 end
 
