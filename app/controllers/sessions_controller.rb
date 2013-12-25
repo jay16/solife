5809b1e647e320c826e6b5d3b23965c69dc6271e
jay16 <jay_li@intfocus.com>
2013-12-25 22:15:02 +0800

    auto commit - 2013/12/25-22:15:01

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,3 +1,4 @@
+#encoding: utf-8
 class SessionsController < Devise::SessionsController
   layout "layout_v2/application"
   respond_to :html, :js
@@ -10,11 +11,13 @@ class SessionsController < Devise::SessionsController
   #登陆后刷新当前界面
   def create
     super
+    flash[:alert] = "您已成功登陆SOLife"
   end
 
   #退出后刷新当前界面
   def destroy
     #super
     reset_session
+    flash[:alert] = "您已成功退出SOLife"
   end
 end
