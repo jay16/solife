fac59c4db3a4be10c35f0b1537d5bbc09af63c78
jay16 <jay_li@intfocus.com>
2014-02-12 22:15:01 +0800

    auto commit - 2014/02/12-22:15:01

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -1,7 +1,7 @@
 #encoding: utf-8
 require "ip_reverse"
 class Api::UsersController < ApplicationController
-delegate "gravatar_image_tag", :to => "ActionController::Base.helpers"
+delegate "gravatar_image_url", :to => "ActionController::Base.helpers"
   
   def info
     ret, ret_info, user_name, user_area, user_gravatar,user_register = 0, "", "", "读取失败", "",""
@@ -10,8 +10,8 @@ delegate "gravatar_image_tag", :to => "ActionController::Base.helpers"
       if user = User.find_by_email(params[:email])
         ret, ret_info =  1, "OK" 
 	user_name = user.name
-	user_gravatar = gravatar_image_tag(params[:email])
-        user_register = user.created_at.stftime("%Y-%-%d %H:%M") #"2013-06-07 07:12"
+	user_gravatar = gravatar_image_url(params[:email])
+        user_register = user.created_at.strftime("%Y-%m-%d %H:%M") #"2013-06-07 07:12"
 	ip_reverse = IpReverse.reverse(request.remote_ip)
 	if ip_reverse['result'] == "success"
           user_area = ip_reverse['country']+ip_reverse['province']
