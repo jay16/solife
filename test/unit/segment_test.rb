2e49eac556aaf8070bbf4b16cbd018d16584ae10
jay16 <jay_li@intfocus.com>
2014-03-06 17:15:01 +0800

    auto commit - 2014/03/06-17:15:01

--- test/unit/segment_test.rb
+++ test/unit/segment_test.rb
@@ -4,9 +4,4 @@ class SegmentTest < ActiveSupport::TestCase
   def setup
   end
 
-  test "should get index" do
-    get :index
-    assert_response :success
-    assert_not_nil assigns(:segments)
-  end
 end
